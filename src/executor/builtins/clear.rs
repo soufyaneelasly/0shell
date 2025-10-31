c70bfabbf238f3f3df4de33643d016fb49e370b0
mmoulabbi <mehdi.nada909@gmail.com>
2025-10-31 17:24:57 +0100

    fix clear command

--- src/executor/builtins/clear.rs
+++ src/executor/builtins/clear.rs
@@ -2,8 +2,8 @@ use std::io::{self, Write};
 use crate::executor::{ExecutionResult, ExecutorError};
 
 pub fn execute(_args: &[String]) -> Result<ExecutionResult, ExecutorError> {
-    print!("\x1B[2J\x1B[H");
-    io::stdout().flush().unwrap();
+    print!("\x1B[2J\x1B[3J\x1B[H");
+    io::stdout().flush().map_err(|e| ExecutorError::IoError(e))?;
     
     Ok(ExecutionResult {
         output: String::new(),
