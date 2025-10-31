7d99a766dbcd63a296bcbdf757b65d879e072e7b
mmoulabbi <mehdi.nada909@gmail.com>
2025-10-31 16:17:30 +0100

    remove unused commands

--- src/executor/builtins/clear.rs
+++
@@ -1,7 +0,0 @@
-// use std::io::{self, Write};
-
-// pub fn cmd_clear(_args: &[&str]) {
-//     // Code ANSI : clear + reposition curseur
-//     print!("\x1B[2J\x1B[H");
-//     io::stdout().flush().unwrap();
-// }
--- src/executor/builtins/file_ops.rs
+++
@@ -1,1 +0,0 @@
-// cp, rm, mv, mkdir
